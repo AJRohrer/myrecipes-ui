7335edefa8a4eee5a44950a67a73f8d343f05352
Andrew Rohrer <andrewjrohrer@gmail.com>
2020-07-21 23:17:09 -0500

    Username and password can now be retrieved from inputs.

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -1,22 +1,46 @@
-import React from "react";
-import { useApiLogin } from "../Hooks/Login";
+import React, { useState } from "react";
 
 function Login(props) {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  const handleUsernameInput = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordInput = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const logUsernameAndPass = () => {
+    console.log(username + " " + password);
+  };
+
   let inputStyles =
     "bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal my-3";
-
-  const tryLogin = (event) =>
-    useApiLogin(props.hostUrl, { username: "AJRohrer", password: "docker" });
 
   return (
     <div className="flex flex-col">
       <div className="text-center">Login</div>
       <div>
-        <input className={inputStyles} type="text" placeholder="Username" />
-        <input className={inputStyles} type="password" placeholder="Password" />
+        <input
+          onChange={handleUsernameInput}
+          className={inputStyles}
+          type="text"
+          placeholder="Username"
+        />
+        <input
+          onChange={handlePasswordInput}
+          className={inputStyles}
+          type="password"
+          placeholder="Password"
+        />
       </div>
 
-      <button onClick={tryLogin} className="bg-blue-500 text-white p-3 w-full">
+      <button
+        onClick={logUsernameAndPass}
+        className="bg-blue-500 text-white p-3 w-full"
+      >
         Login
       </button>
     </div>
